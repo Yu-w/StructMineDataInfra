835a27051166070da68659c597fc1596775f625e
mickeystroller <mickeysjm@gmail.com>
2017-07-29 22:47:21 -0700

    [network vis] add sample network visualization query

---
+++ network_vis_sample_query.py
@@ -0,0 +1,17 @@
+'''
+__author__: Jiaming Shen
+__description__: Save the network visualization sample query
+'''
+## Add sample network visualization query as following format
+QUERY_NET = [
+    {
+        "argument1": "Chemicals_and_Drugs",
+        "argument2": "Anatomy",
+        "relation": "is_associated_anatomy_of_gene_product"
+    },
+    {
+        "argument1": "Nervous_System",
+        "argument2": "Anatomy",
+        "relation": "has_nerve_supply"
+    }
+]
