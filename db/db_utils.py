d726e1b5b14fcccf26de8f7a7376f77021c2d236
GentleZhu <544524199@qq.com>
2017-07-30 13:39:26 -0500

    multi edge papers

--- db/db_utils.py
+++ db/db_utils.py
@@ -2,7 +2,7 @@ import json
 import sys
 from pg import DB
 import marshal
-import multiprocessing
+from multiprocessing.dummy import Pool as ThreadPool
 from collections import defaultdict
 import ast
 import random,string
@@ -15,8 +15,8 @@ class data_utils(object):
 		#super(db_utils, self).__init__()
 		self.arg = arg
 		self.db = DB(dbname='structnet', user='structnet', passwd='structnet', host='localhost')
-		self.identity = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
-		print self.identity
+		#self.identity = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
+		#print self.identity
 		print "Conncted!"
 
 	def load_pmids(self):
@@ -187,10 +187,10 @@ class data_utils(object):
 		type_a = ast.literal_eval(type_a)
 		type_b = ast.literal_eval(type_b)
 		#print num_edges
-		query_string = "SELECT * FROM (SELECT DISTINCT ON(entity_a,entity_b) entity_a,entity_b,sent_id  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
-		+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "') x ORDER BY RANDOM() LIMIT 500"
-		#query_string = "SELECT * FROM (SELECT entity_a,entity_b,(array_agg('[' || article_id || ',' || sent_id || ']'))[1:" + str(num_pps) + "]  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
-		#+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "' GROUP BY entity_a,entity_b) x ORDER BY RANDOM() LIMIT " +str(num_edges)
+		#query_string = "SELECT * FROM (SELECT DISTINCT ON(entity_a,entity_b) entity_a,entity_b,sent_id  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
+		#+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "') x ORDER BY RANDOM() LIMIT 500"
+		query_string = "SELECT * FROM (SELECT entity_a,entity_b,(array_agg('[' || article_id || ',' || sent_id || ']'))[1:" + str(num_pps) + "]  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
+		+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "' GROUP BY entity_a,entity_b) x ORDER BY RANDOM() LIMIT " +str(num_edges)
 		q = self.db.query(query_string)
 		result={'node_a': {}, 'node_b': {}, 'edges': []}
 		for p in list(self.generate_random_walks(q.dictresult())):
@@ -204,56 +204,52 @@ class data_utils(object):
 		#type_a={'mesh':0, 'name':"Chemicals_and_Drugs"}
 		type_a = ast.literal_eval(type_a)
 		type_b = ast.literal_eval(type_b)
-		#print type_a
-		#print type_b
-		#print relation_type
-		#print query_string
-		#q = self.db.query("select exists(select relname from pg_class where relname = 'temp_network' and relkind='r')")
-		#if q.dictresult()[0]['exists']:
-		query_string = "SELECT * INTO " +self.identity+ " FROM (SELECT DISTINCT ON(entity_a,entity_b) entity_a,entity_b,sent_id  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
-		+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "') x ORDER BY RANDOM() LIMIT " +str(num_edges)
-		
-		#print query_string
-		'''
-		query_string = "SELECT R.entity_a,R.entity_b,(array_agg( DISTINCT (E1.pmid,E1.article_title)))[1:3] AS a_id,(array_agg(DISTINCT (E2.sent_id,E2.pmid,E2.article_title)))[1:3] AS b_id "+\
-		"FROM " + \
-		"(SELECT "+"DISTINCT entity_a,entity_b "+"FROM "+self.arg['entity_table']+" WHERE type_a_mesh["+type_a['mesh']+ \
-		"]='"+type_a['name']+"' AND type_b_mesh["+type_b['mesh']+"]='"+ type_b['name']+"' AND relation_type='"+relation_type + "' LIMIT 10) R" +\
-		" INNER JOIN sample_entity_table E1 ON E1.entity_name = R.entity_a" + \
-		" INNER JOIN sample_entity_table E2 ON E2.entity_name = R.entity_b GROUP BY R.entity_a,R.entity_b"
-		'''
-		
-		self.db.query(query_string)
-		query_a=self.db.query("select entity_a from "+self.identity)
-		#print ttt.dictresult()
+		#query_string = "SELECT * INTO " +self.identity+ " FROM (SELECT DISTINCT ON(entity_a,entity_b) entity_a,entity_b,sent_id  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
+		#+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "') x ORDER BY RANDOM() LIMIT " +str(num_edges)
+		query_string_v2 = "SELECT * FROM (SELECT entity_a,entity_b,(array_agg('[' || article_id || ',' || sent_id || ']'))[1:" + str(num_pps+2) + "] as sents  "+"FROM "+self.arg['relation_table']+" WHERE type_a_"+type_a['name']+"@>'"\
+		+type_a['type']+"' AND type_b_"+type_b['name']+"@>'"+ type_b['type']+"' AND relation_type='"+relation_type + "' GROUP BY entity_a,entity_b) x ORDER BY RANDOM() LIMIT " +str(num_edges)
+		q = self.db.query(query_string_v2)
+		result = q.dictresult()
+		query_a = []
+		query_b = []
+		query_edges = []
+		for r in result:
+			query_a.append(r['entity_a'])
+			query_b.append(r['entity_b'])
+			article = dict()
+			for x in r['sents']:
+				x = ast.literal_eval(x)
+				article[x[0]]=str(x[1])
+			query_edges.append({'source':r['entity_a'], 'target':r['entity_b'], 'sids':article.values(), 'sents':[]})
+
 		red_node = dict()
-		for v in query_a.dictresult():
+		for v in query_a:
 			#print "select distinct on (article_id) article_title, pmid, sent from entity_table where entity_name= '" + v['entity_a'] + "' LIMIT "+str(num_pps)
 			#print "select distinct on (article_id) article_title, pmid, sent from entity_table where entity_name= '" + v['entity_a'] + "' LIMIT 2" 
-			tmp=self.db.query("select article_title, pmid, sent from entity_table where entity_name= '" + v['entity_a'] + "' LIMIT "+str(num_pps))
+			tmp=self.db.query("select article_title, pmid, sent from entity_table where entity_name= '" + v + "' LIMIT "+str(num_pps))
 			#tmp=self.db.query("select distinct on (article_id) article_title, pmid, sent from entity_table where entity_name= '" + v['entity_a'] + "' LIMIT "+str(num_pps))
-			red_node[v['entity_a']] = map(lambda x:(x['article_title'],x['sent'],x['pmid']),tmp.dictresult())
-		query_b=self.db.query("select entity_b from "+self.identity)
+			red_node[v] = map(lambda x:(x['article_title'],x['sent'],x['pmid']),tmp.dictresult())
+		#query_b=self.db.query("select entity_b from "+self.identity)
 		blue_node = dict()
-		for v in query_b.dictresult():
+		for v in query_b:
 			#pass
 			#print "select distinct on (article_id) article_title, pmid, sent from entity_table where entity_name= '" + v['entity_b'] + "' LIMIT "+str(num_pps)
-			tmp=self.db.query("select article_title, pmid, sent from entity_table where entity_name= '" + v['entity_b'] + "' LIMIT "+str(num_pps))
+			tmp=self.db.query("select article_title, pmid, sent from entity_table where entity_name= '" + v + "' LIMIT "+str(num_pps))
 			#tmp=self.db.query("select distinct on (article_id) article_title, pmid, sent from entity_table where entity_name= '" + v['entity_b'] + "' LIMIT "+str(num_pps))
-			blue_node[v['entity_b']] = map(lambda x:(x['article_title'],x['sent'],x['pmid']),tmp.dictresult())
+			blue_node[v] = map(lambda x:(x['article_title'],x['sent'],x['pmid']),tmp.dictresult())
 		#print red_node,blue_node
-		query_edge = "SELECT DISTINCT T.entity_a as source, T.entity_b as target, E.pmid, E.article_title,E.sent FROM " + self.arg['entity_table'] + " AS E INNER JOIN "+\
-		self.identity+" T ON E.sent_id = T.sent_id";
-		#query_edge = "SELECT distinct on (R.article_id) R.article_id, R.sent_id from " + self.arg['relation_table']+ " AS R inner join " + self.identity+  \
+		for edge in query_edges:
+			for s_id in edge['sids'][:num_pps]:
+				tmp = self.db.query("select article_title, pmid, sent from entity_table where sent_id= '" + s_id + "' LIMIT 1")
+				edge['sents'].append(tmp.dictresult()[0])
+		#query_edge = "SELECT DISTINCT T.entity_a as source, T.entity_b as target, E.pmid, E.article_title,E.sent FROM " + self.arg['entity_table'] + " AS E INNER JOIN "+\
+		#self.identity+" T ON E.sent_id = T.sent_id";
+		#query_edges = "SELECT distinct on (R.article_id) R.article_id, R.sent_id from " + self.arg['relation_table']+ " AS R inner join " + self.identity+  \
 		#" T on R.entity_a = T.entity_a and R.entity_b = T.entity_b and R.relation_type = \'" + relation_type +"' LIMIT " +str(num_pps)
 		#"distinct on (article_id) inner join on entity_a, entity_b, relation_type"
-		q = self.db.query(query_edge)
-		self.db.query("drop table "+self.identity)
-		return {'node_a':red_node,'node_b':blue_node,'edge':q.dictresult()}
-		#print query_red_node.dictresult()
-		#q = self.db.query(query_red_node)
-		#print q.dictresult()
-		#return q.dictresult()
+		#q = self.db.query(query_edge)
+		#self.db.query("drop table "+self.identity)
+		return {'node_a':red_node,'node_b':blue_node,'edge':query_edges}
 
 #class db_utils(object)
 if __name__ == '__main__':
@@ -267,10 +263,11 @@ if __name__ == '__main__':
 	if sys.argv[1] == 'query':
 		if sys.argv[2] == 'network':
 			tmp_utils = data_utils({'entity_table': sys.argv[3], 'relation_table': sys.argv[4]})
-			result = tmp_utils.query_links(type_a=sys.argv[5], type_b=sys.argv[6], relation_type=sys.argv[7], num_edges=sys.argv[8], num_pps=sys.argv[9])
+			result = tmp_utils.query_links(type_a=sys.argv[5], type_b=sys.argv[6], relation_type=sys.argv[7], num_edges=sys.argv[8], num_pps=int(sys.argv[9]))
+			print result
 		elif sys.argv[2] == 'connected_network':
 			tmp_utils = data_utils({'entity_table': sys.argv[3], 'relation_table': sys.argv[4]})
-			result = tmp_utils.query_links_with_walk(type_a=sys.argv[5], type_b=sys.argv[6], relation_type=sys.argv[7], num_edges=sys.argv[8], num_pps=sys.argv[9])
+			result = tmp_utils.query_links_with_walk(type_a=sys.argv[5], type_b=sys.argv[6], relation_type=sys.argv[7], num_edges=sys.argv[8], num_pps=int(sys.argv[9]))
 		elif sys.argv[2] == 'predict':
 			tmp_utils = data_utils({'prediction_table': sys.argv[3]})
 			result = tmp_utils.query_prediction(name_a=sys.argv[4], name_b=sys.argv[5], relation_type=sys.argv[6])
