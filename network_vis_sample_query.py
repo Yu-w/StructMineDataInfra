ac4e0c22e5cd50bf6dc551a9d3171765fbd7433e
mickeystroller <mickeysjm@gmail.com>
2017-07-31 08:46:35 -0700

    [network vis] change back the argument positions

--- network_vis_sample_query.py
+++ network_vis_sample_query.py
@@ -5,58 +5,58 @@ __description__: Save the network visualization sample query
 ## Add sample network visualization query as following format
 QUERY_NET = [
     {
-        "argument2": "Chromosomes",
-        "argument1": "Diseases",
+        "argument1": "Chromosomes",
+        "argument2": "Diseases",
         "relation": "cytogenetic_abnormality_involves_chromosome"
     },
     {
-        "argument2": "Peripheral_Nerves",
-        "argument1": "Anatomy",
+        "argument1": "Peripheral_Nerves",
+        "argument2": "Anatomy",
         "relation": "has_nerve_supply"
     },
     {
-        "argument2": "Veins",
-        "argument1": "Anatomy",
+        "argument1": "Veins",
+        "argument2": "Anatomy",
         "relation": "has_venous_drainage"
     },
     {
-        "argument2": "Proteins",
-        "argument1": "Psychiatry_and_Psychology",
+        "argument1": "Proteins",
+        "argument2": "Psychiatry_and_Psychology",
         "relation": "associated_with_malfunction_of_gene_product"
     },
     {
-        "argument2": "Antifungal_Agents",
-        "argument1": "Chemicals_and_Drugs",
+        "argument1": "Antifungal_Agents",
+        "argument2": "Chemicals_and_Drugs",
         "relation": "has_therapeutic_class"
     },
     {
-        "argument2": "Genetic_Phenomena",
-        "argument1": "Diseases",
+        "argument1": "Genetic_Phenomena",
+        "argument2": "Diseases",
         "relation": "disease_may_have_cytogenetic_abnormality"
     },
     {
-        "argument2": "Chemical_Actions_and_Uses",
-        "argument1": "Chemicals_and_Drugs",
+        "argument1": "Chemical_Actions_and_Uses",
+        "argument2": "Chemicals_and_Drugs",
         "relation": "has_therapeutic_class"
     },
     {
-        "argument2": "Hematologic_Diseases",
-        "argument1": "Phenomena_and_Processes",
+        "argument1": "Hematologic_Diseases",
+        "argument2": "Phenomena_and_Processes",
         "relation": "is_not_cytogenetic_abnormality_of_disease"
     },
     {
-        "argument2": "Anti-Arrhythmia_Agents",
-        "argument1": "Chemicals_and_Drugs",
+        "argument1": "Anti-Arrhythmia_Agents",
+        "argument2": "Chemicals_and_Drugs",
         "relation": "has_therapeutic_class"
     },
     {
-        "argument2": "Genetic_Variation",
-        "argument1": "Diseases",
+        "argument1": "Genetic_Variation",
+        "argument2": "Diseases",
         "relation": "disease_may_have_cytogenetic_abnormality"
     },
     {
-        "argument2": "Lymphoproliferative_Disorders",
-        "argument1": "Anatomy",
+        "argument1": "Lymphoproliferative_Disorders",
+        "argument2": "Anatomy",
         "relation": "is_not_abnormal_cell_of_disease"
     }
 ]
