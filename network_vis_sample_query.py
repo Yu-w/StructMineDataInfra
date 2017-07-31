21a67be98ed423d4fe57e44091853462ad58f04b
xuanwang91 <LilyBiology2010@gmail.com>
2017-07-30 19:25:44 -0500

    Update network_vis_sample_query.py

--- network_vis_sample_query.py
+++ network_vis_sample_query.py
@@ -5,14 +5,9 @@ __description__: Save the network visualization sample query
 ## Add sample network visualization query as following format
 QUERY_NET = [
     {
-        "argument1": "Chemicals_and_Drugs",
-        "argument2": "Anatomy",
-        "relation": "is_associated_anatomy_of_gene_product"
-    },
-    {
-        "argument1": "Genetic_Phenomena",
+        "argument1": "Chromosomes",
         "argument2": "Diseases",
-        "relation": "disease_may_have_cytogenetic_abnormality"
+        "relation": "cytogenetic_abnormality_involves_chromosome"
     },
     {
         "argument1": "Peripheral_Nerves",
@@ -20,8 +15,38 @@ QUERY_NET = [
         "relation": "has_nerve_supply"
     },
     {
-        "argument1": "Chromosomes",
+        "argument1": "Veins",
+        "argument2": "Anatomy",
+        "relation": "has_venous_drainage"
+    },
+    {
+        "argument1": "Proteins",
+        "argument2": "Psychiatry_and_Psychology",
+        "relation": "associated_with_malfunction_of_gene_product"
+    },
+    {
+        "argument1": "Antifungal_Agents",
+        "argument2": "Chemicals_and_Drugs",
+        "relation": "has_therapeutic_class"
+    },
+    {
+        "argument1": "Genetic_Phenomena",
         "argument2": "Diseases",
-        "relation": "cytogenetic_abnormality_involves_chromosome"
+        "relation": "disease_may_have_cytogenetic_abnormality"
+    },
+    {
+        "argument1": "Chemical_Actions_and_Uses",
+        "argument2": "Chemicals_and_Drugs",
+        "relation": "has_therapeutic_class"
+    },
+    {
+        "argument1": "Hematologic_Diseases",
+        "argument2": "Phenomena_and_Processes",
+        "relation": "is_not_cytogenetic_abnormality_of_disease"
+    },
+    {
+        "argument1": "Anti-Arrhythmia_Agents",
+        "argument2": "Chemicals_and_Drugs",
+        "relation": "has_therapeutic_class"
     }
 ]
