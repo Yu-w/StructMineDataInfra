ee46076f9079acf14942ecd4035987d696a97c9f
Yu Wang <yuwang17@illinois.edu>
2017-11-09 16:27:54 -0600

    active step wording

--- www/src/routes/exploration/Exploration.js
+++ www/src/routes/exploration/Exploration.js
@@ -21,55 +21,37 @@ class Exploration extends React.Component {
   }
 
   render() {
-    const leftLinkComponent = (
-      <a
-        href="#"
-        onClick={(event) => {
-          event.preventDefault();
-        }}
-        >(example)</a>
+    return (
+      <div
+        className={s.root}
+        style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+      >
+        <div className={s.container}>
+          <span className={s.title} >Life-iNet</span>
+          <SearchBar
+            style={{marginTop: 8}}
+            onActiveStepChange={(activeStep) => this.setState({activeStep: activeStep})}
+          />
+          <Stepper activeStep={this.state.activeStep}>
+            <Step>
+              <StepLabel>
+                Select Left Category with Entities your choice
+              </StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>
+                Select Right Category with Entities your choice
+              </StepLabel>
+            </Step>
+            <Step>
+              <StepLabel>Choose Relationship between Entities</StepLabel>
+            </Step>
+          </Stepper>
+        </div>
+        </div>
       );
-      const rightLinkComponent = this.state.activeStep >= 1 ? (
-        <a
-          href="#"
-          onClick={(event) => {
-            event.preventDefault();
-          }}
-          >(example)</a>
-        ) : null;
-        return (
-          <div
-            className={s.root}
-            style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
-          >
-            <div className={s.container}>
-              <span className={s.title} >Life-iNet</span>
-              <SearchBar
-                style={{marginTop: 8}}
-                onActiveStepChange={(activeStep) => this.setState({activeStep: activeStep})}
-              />
-              <Stepper activeStep={this.state.activeStep}>
-                <Step>
-                  <StepLabel>
-                    Select Left Entities &nbsp;
-                    {leftLinkComponent}
-                  </StepLabel>
-                </Step>
-                <Step>
-                  <StepLabel>
-                    Select Right Entities &nbsp;
-                    {rightLinkComponent}
-                  </StepLabel>
-                </Step>
-                <Step>
-                  <StepLabel>Choose Relationship between Entities</StepLabel>
-                </Step>
-                </Stepper>
-              </div>
-            </div>
-          );
-        }
+    }
 
-      }
+  }
 
-      export default withStyles(s)(Exploration);
+  export default withStyles(s)(Exploration);
