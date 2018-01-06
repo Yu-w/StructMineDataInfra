e29fec8fb50f77d27733623be18ab03acfa5d769
Yu Wang <wangyu2165@gmail.com>
2018-01-06 17:09:29 -0600

    i hate nvm

--- www/deploy.sh
+++ www/deploy.sh
@@ -4,9 +4,12 @@ echo The following happened on remote server:
 
 ssh wangyu2165@73.222.137.165 $(cat <<EOF
     set -e;
+    echo "Loading NVM...";
+    export NVM_DIR="\$HOME/.nvm";
+    . "\$NVM_DIR/nvm.sh";
     echo Pulling Git changes;
     cd \$HOME/StructMineDataInfra/www;
     git pull;
-    /home/wangyu2165/.nvm/versions/node/v8.9.4/bin/pm2 restart pm2-service.json;
+    pm2 restart pm2-service.json;
     echo Done.
 EOF)
